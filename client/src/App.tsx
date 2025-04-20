8f665be328c040e4d0309ee11e8fa5221f114a2a
Karan Bhalla <karanbhalla204@gmail.com>
2025-04-20 13:33:14 -0500

    test connection b/w frontend and backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,48 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './index.css'
+
+const BACKEND_URL = import.meta.env.VITE_API_URL
 
 function App() {
 
-  return (
-    <div className = "text-2xl font-extrabold text-center my-4">
-        HELLO
+    const [word, setWord] = useState<string>()
+
+    const test = async() => {
+        try {
+            const response = await fetch(`${BACKEND_URL}/api`)
+            if (!response.ok){
+                const errorMessage = await response.text()
+                throw new Error(errorMessage)
+            }
+
+            const data = await response.json()
+            setWord(data)
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+      
+        test()
+      
+    }, [])
+    
+
+    
+    return (
+    <div>
+        <div className = "text-2xl font-extrabold text-center my-4">
+            HELLO
+        </div>
+
+        <div className='text-xl text-blue-700 text-center my-96'>
+            {word}
+        </div>
+
     </div>
-  )
+    )
 }
 
 export default App
