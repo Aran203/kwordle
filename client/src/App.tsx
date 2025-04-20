725a88e3e5d7b592d1da2d48a447feb8504ed5bb
Karan Bhalla <karanbhalla204@gmail.com>
2025-04-20 01:05:05 -0500

    Setup vite project

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className = "text-2xl font-extrabold text-center my-4">
+        HELLO
+    </div>
+  )
+}
+
+export default App
