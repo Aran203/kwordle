ebba026e1f979e5089f0e7c5336cef28c601e676
Karan Bhalla <karanbhalla204@gmail.com>
2025-04-21 03:19:52 -0500

    Add frontend cors to backend

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,8 @@ import {validateWord} from './routes/words.js'
 const app = express()
 const PORT = 8080
 
-app.use(cors())
-app.use(express.json())
+app.use(cors({ origin: 'https://kwordled.vercel.app', methods: ['GET', 'POST'] }));
+app.use(express.json());
 
 
 app.get('/api', (request, response) => {
